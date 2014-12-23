2c507197d9888bda89d97e5882e98a59e7fcb6b2
Mateusz Sikora <m.sikora@skytale.pl>
2014-12-23 13:45:11 +0100

    v0.0.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ f.close()
 
 setup(
     name='django-rest-common',
-    version='0.0.4',
+    version='0.0.5',
     author='Sumit Chachra',
     author_email='chachra@tivix.com',
     url='http://github.com/Tivix/django-rest-common',
